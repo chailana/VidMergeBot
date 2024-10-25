fdec0b2f12c21062ac27d847fb1063465c32805c
chailana <chailana35217793@gmail.com>
2024-10-25 17:34:06 +0530

    Update __main__.py
    Signed-off-by: chailana <chailana35217793@gmail.com>

--- vidmergebot/__main__.py
+++ vidmergebot/__main__.py
@@ -1,4 +1,6 @@
 from vidmergebot.bot_class import VidMergeBot
 
 if __name__ == "__main__":
-    VidMergeBot().run()
+    # Assuming VidMergeBot has a method to set the port or an init parameter
+    bot = VidMergeBot()
+    bot.run(port=8000)  # Replace this line if `run` method needs different arguments
